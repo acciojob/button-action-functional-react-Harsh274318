bc4ee94e308c0c3014e43c9c7d40d70f3922a653
Harsh Vardhan Pal <hp77335@gmail.com>
2026-01-29 07:34:11 +0530

    First Try

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isture, setIstrue] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
       <h1>hello</h1>
+      <button id="click" onClick={() => setIstrue(true)}>
+        click
+      </button>
+      {isture && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
